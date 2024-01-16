9785437008910118eb17570b049e3d5e609d4578
Marek Skopal <skopal.marek@gmail.com>
2024-01-16 14:23:19 +0100

    Add range switcher

--- frontend/src/app/history/components/history/history.component.ts
+++ frontend/src/app/history/components/history/history.component.ts
@@ -1,24 +1,15 @@
-﻿import { Component, OnInit } from '@angular/core';
-import { first } from 'rxjs/operators';
-
-import {CurrencyService, GroupWithGroupDataService} from '@app/services';
-import {Currency, GroupWithGroupData} from "@app/models";
+﻿import { Component } from '@angular/core';
+import {PortfolioDataRangeEnum} from "@app/models";
 
 @Component({ templateUrl: 'history.component.html' })
-export class HistoryComponent implements OnInit {
-    public groupsWithGroupData: GroupWithGroupData[]|null = null;
-    public defaultCurrency: Currency;
+export class HistoryComponent {
 
-    public constructor(
-        private groupWithGroupDataService: GroupWithGroupDataService,
-        private currencyService: CurrencyService,
-    ) {}
+    public range: PortfolioDataRangeEnum = PortfolioDataRangeEnum.SevenDays;
 
-    public async ngOnInit(): Promise<void> {
-        this.defaultCurrency = await this.currencyService.getDefaultCurrency();
-
-        this.groupWithGroupDataService.getGroupWithGroupData()
-            .pipe(first())
-            .subscribe((groupsWithGroupData: GroupWithGroupData[]) => this.groupsWithGroupData = groupsWithGroupData);
+    public changeRange(range: PortfolioDataRangeEnum) {
+        this.range = range;
+        console.log(this.range);
     }
+
+    protected readonly PortfolioDataRangeEnum = PortfolioDataRangeEnum;
 }
