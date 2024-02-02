6905a05aed8140a044af5cdf230f56c081689303
Marek Skopal <skopal.marek@gmail.com>
2024-02-02 21:19:00 +0100

    Add cs translate

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -14,6 +14,7 @@ import {TranslateModule} from "@ngx-translate/core";
     imports: [
         CommonModule,
         ReactiveFormsModule,
+        TranslateModule
     ],
     declarations: [
         PortfolioTotalComponent,
