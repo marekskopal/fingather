81afdea21586b2fffb736d1c0d1eaf999b3d4cda
Marek Skopal <skopal.marek@gmail.com>
2024-05-18 21:00:34 +0200

    Add detail cards

--- frontend/src/app/assets/components/detail/detail.component.ts
+++ frontend/src/app/assets/components/detail/detail.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AssetWithProperties } from '@app/models';
-import { AssetService } from '@app/services';
+import { AssetWithProperties, Currency } from '@app/models';
+import { AssetService, CurrencyService } from '@app/services';
 
 @Component({ templateUrl: 'detail.component.html' })
 export class DetailComponent implements OnInit {
-    public asset: AssetWithProperties | null = null;
-    public id: number;
+    protected asset: AssetWithProperties | null = null;
+    protected defaultCurrency: Currency;
+    private id: number;
 
     public constructor(
-        private assetService: AssetService,
-        private route: ActivatedRoute,
+        private readonly assetService: AssetService,
+        private readonly currencyService: CurrencyService,
+        private readonly route: ActivatedRoute,
     ) {}
 
     public async ngOnInit(): Promise<void> {
         this.id = this.route.snapshot.params['id'];
 
         this.asset = await this.assetService.getAsset(this.id);
+        this.defaultCurrency = await this.currencyService.getDefaultCurrency();
     }
 }
--- frontend/src/app/models/ticker.ts
+++ frontend/src/app/models/ticker.ts
@@ -1,3 +1,4 @@
+import { TickerTypeEnum } from '@app/models/enums/ticker-type-enum';
 import { Market } from '@app/models/market';
 
 export interface Ticker {
@@ -6,6 +7,13 @@ export interface Ticker {
     name: string;
     marketId: number;
     currencyId: number;
+    type: TickerTypeEnum;
+    isin: string | null;
     logo: string | null;
+    sector: string | null;
+    industry: string | null;
+    website: string | null;
+    description: string | null;
+    country: string | null;
     market: Market;
 }
