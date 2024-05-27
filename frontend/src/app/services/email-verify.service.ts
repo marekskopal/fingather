2162c81e4fee62c6ed7163f15f2e3d95f2c4acb2
Marek Skopal <skopal.marek@gmail.com>
2024-05-27 17:49:11 +0200

    Remove notify extend

--- frontend/src/app/services/email-verify.service.ts
+++ frontend/src/app/services/email-verify.service.ts
@@ -1,16 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { OkResponse } from '@app/models/ok-response';
-import { NotifyService } from '@app/services/notify-service';
 import { environment } from '@environments/environment';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export class EmailVerifyService extends NotifyService {
+export class EmailVerifyService {
     public constructor(
         private readonly http: HttpClient,
     ) {
-        super();
     }
 
     public verifyEmail(token: string): Promise<OkResponse> {
