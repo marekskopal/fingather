4beeac1bc9fa470811cd9e16b6f79d9e8a2dcb49
Marek Skopal <skopal.marek@gmail.com>
2024-08-25 20:21:27 +0200

    Refactor transactions list

--- frontend/src/app/assets/components/detail/components/transactions/transaction-list.component.ts
+++ frontend/src/app/assets/components/detail/components/transactions/transaction-list.component.ts
@@ -1,72 +1,26 @@
 import {
-    ChangeDetectionStrategy, Component, OnInit, signal
+    ChangeDetectionStrategy, Component
 } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Transaction, TransactionActionType } from '@app/models';
-import { PortfolioService, TransactionService } from '@app/services';
-import { ConfirmDialogService } from '@app/services/confirm-dialog.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+    BaseTransactionListComponent
+} from "@app/assets/components/detail/components/transactions/base-transaction-list.component";
+import { TransactionActionType} from '@app/models';
+import {TransactionList} from "@app/models/transaction-list";
 
 @Component({
     templateUrl: 'transaction-list.component.html',
     selector: 'fingather-transaction-list',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TransactionListComponent implements OnInit {
-    private $transactions = signal<Transaction[] | null>(null);
-    public assetId: number;
-
-    public constructor(
-        private readonly transactionService: TransactionService,
-        private readonly portfolioService: PortfolioService,
-        private route: ActivatedRoute,
-        private modalService: NgbModal,
-        private readonly confirmDialogService: ConfirmDialogService,
-    ) {}
-
-    public ngOnInit(): void {
-        this.assetId = this.route.snapshot.params['id'];
-
-        this.refreshTransactions();
-
-        this.transactionService.subscribe(() => {
-            this.refreshTransactions();
-        });
-    }
-
-    public async refreshTransactions(): Promise<void> {
-        const portfolio = await this.portfolioService.getCurrentPortfolio();
-
-        const transactions = await this.transactionService.getTransactions(portfolio.id, this.assetId, [
-            TransactionActionType.Buy,
-            TransactionActionType.Sell
-        ]);
-
-        this.$transactions.set(transactions.transactions);
-    }
-
-    protected get transactions(): Transaction[] | null {
-        return this.$transactions();
-    }
-
-    public addTransaction(assetId: number): void {
-        //const transactionDialogComponent = this.modalService.open(TransactionDialogComponent);
-        //transactionDialogComponent.componentInstance.assetId = assetId;
-    }
-
-    public editTransaction(id: number): void {
-        //const transactionDialogComponent = this.modalService.open(TransactionDialogComponent);
-        //transactionDialogComponent.componentInstance.id = id;
-    }
-
-    public async deleteTransaction(id: number): Promise<void> {
-        const transaction = this.$transactions()?.find((x) => x.id === id);
-        if (transaction === undefined) {
-            return;
-        }
-
-        await this.transactionService.deleteTransaction(id);
-
-        this.refreshTransactions();
+export class TransactionListComponent extends BaseTransactionListComponent {
+    protected async getTransactions(portfolioId: number): Promise<TransactionList> {
+        return await this.transactionService.getTransactions(
+            portfolioId,
+            this.$asset().id,
+            [
+                TransactionActionType.Buy,
+                TransactionActionType.Sell
+            ]
+        );
     }
 }
