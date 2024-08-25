8115ab750ada431700f683729fb13a863b84f80d
Marek Skopal <skopal.marek@gmail.com>
2024-08-25 20:21:27 +0200

    Refactor transactions list

--- frontend/src/app/assets/components/detail/components/dividends/dividend-list.component.ts
+++ frontend/src/app/assets/components/detail/components/dividends/dividend-list.component.ts
@@ -1,59 +1,26 @@
 import {
-    ChangeDetectionStrategy, Component, OnInit, signal
+    ChangeDetectionStrategy, Component
 } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Transaction, TransactionActionType } from '@app/models';
-import { PortfolioService, TransactionService } from '@app/services';
+import {
+    BaseTransactionListComponent
+} from "@app/assets/components/detail/components/transactions/base-transaction-list.component";
+import { TransactionActionType} from '@app/models';
+import {TransactionList} from "@app/models/transaction-list";
 
 @Component({
     templateUrl: 'dividend-list.component.html',
     selector: 'fingather-dividend-list',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DividendListComponent implements OnInit {
-    private $dividends = signal<Transaction[] | null>(null);
-    public assetId: number;
-
-    public constructor(
-        private readonly transactionService: TransactionService,
-        private readonly portfolioService: PortfolioService,
-        private route: ActivatedRoute,
-    ) {}
-
-    public ngOnInit(): void {
-        this.assetId = this.route.snapshot.params['id'];
-
-        this.refreshTransactions();
-
-        this.transactionService.subscribe(() => {
-            this.refreshTransactions();
-        });
-    }
-
-    public async refreshTransactions(): Promise<void> {
-        const portfolio = await this.portfolioService.getCurrentPortfolio();
-
-        const transactions = await this.transactionService.getTransactions(
-            portfolio.id,
-            this.assetId,
-            [TransactionActionType.Dividend]
+export class DividendListComponent extends BaseTransactionListComponent {
+    protected async getTransactions(portfolioId: number): Promise<TransactionList> {
+        return await this.transactionService.getTransactions(
+            portfolioId,
+            this.$asset().id,
+            [
+                TransactionActionType.Dividend,
+                TransactionActionType.DividendTax,
+            ]
         );
-
-        this.$dividends.set(transactions.transactions);
-    }
-
-    protected get dividends(): Transaction[] | null {
-        return this.$dividends();
-    }
-
-    protected async deleteDividend(id: number): Promise<void> {
-        const transaction = this.$dividends()?.find((x) => x.id === id);
-        if (transaction === undefined) {
-            return;
-        }
-
-        await this.transactionService.deleteTransaction(id);
-
-        this.refreshTransactions();
     }
 }
