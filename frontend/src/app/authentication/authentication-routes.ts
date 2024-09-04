8830e6ade239c488b7f16f586014698a5a5751eb
Marek Skopal <skopal.marek@gmail.com>
2024-09-04 17:23:30 +0200

    Refactored auth module to standalone

---
+++ frontend/src/app/authentication/authentication-routes.ts
@@ -0,0 +1,21 @@
+import {Route} from '@angular/router';
+import { LayoutComponent } from '@app/authentication/components/layout/layout.component';
+import { LoginComponent } from '@app/authentication/components/login/login.component';
+import { SignUpComponent } from '@app/authentication/components/sign-up/sign-up.component';
+
+export default [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [
+            {
+                path: 'login',
+                component: LoginComponent
+            },
+            {
+                path: 'sign-up',
+                component: SignUpComponent
+            }
+        ]
+    }
+] satisfies Route[];
