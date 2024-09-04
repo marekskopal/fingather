85b1c3ea13808c0758653c828034d7d0c123a93d
Marek Skopal <skopal.marek@gmail.com>
2024-09-04 17:51:03 +0200

    Refactored overviews module to standalone

--- frontend/src/app/overviews/components/list/list.component.ts
+++ frontend/src/app/overviews/components/list/list.component.ts
@@ -1,13 +1,27 @@
+import {AsyncPipe, DecimalPipe} from "@angular/common";
 import {
     ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnInit, signal
 } from '@angular/core';
 import { Currency, YearCalculatedData } from '@app/models';
 import { ModeEnum } from '@app/overviews/components/list/enum/mode-enum';
 import { CurrencyService, OverviewService, PortfolioService } from '@app/services';
+import {PortfolioSelectorComponent} from "@app/shared/components/portfolio-selector/portfolio-selector.component";
+import {TableValueComponent} from "@app/shared/components/table-value/table-value.component";
+import {CurrencyPipe} from "@app/shared/pipes/currency.pipe";
+import {TranslateModule} from "@ngx-translate/core";
 
 @Component({
     templateUrl: './list.component.html',
+    standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        TranslateModule,
+        PortfolioSelectorComponent,
+        TableValueComponent,
+        DecimalPipe,
+        CurrencyPipe,
+        AsyncPipe
+    ]
 })
 export class ListComponent implements OnInit {
     private readonly overviewService = inject(OverviewService);
