5897bbb29aa0c112ec32e3a3b0203e06ebf8b68d
Marek Skopal <skopal.marek@gmail.com>
2024-09-04 13:11:46 +0200

    Fix lint

--- frontend/src/app/users/users.module.ts
+++ frontend/src/app/users/users.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import {MatIcon} from "@angular/material/icon";
+import {InputValidatorComponent} from "@app/shared/components/input-validator/input-validator.component";
+import {SaveButtonComponent} from "@app/shared/components/save-button/save-button.component";
 import { SharedModule } from '@app/shared/shared.module';
 import { AddEditUserComponent } from '@app/users/add-edit/add-edit-user.component';
 import { LayoutComponent } from '@app/users/layout/layout.component';
@@ -16,6 +18,8 @@ import { UsersRoutingModule } from './users-routing.module';
         UsersRoutingModule,
         SharedModule,
         MatIcon,
+        SaveButtonComponent,
+        InputValidatorComponent,
     ],
     declarations: [
         LayoutComponent,
