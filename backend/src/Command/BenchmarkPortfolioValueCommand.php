5f61ad1930064a731d30471de2587b43eaff6c0c
Marek Skopal <skopal.marek@gmail.com>
2024-10-03 00:04:23 +0200

    Refactor portfolio value benchmark to controller call

--- backend/src/Command/BenchmarkPortfolioValueCommand.php
+++ backend/src/Command/BenchmarkPortfolioValueCommand.php
@@ -5,17 +5,14 @@ declare(strict_types=1);
 namespace FinGather\Command;
 
 use FinGather\App\ApplicationFactory;
+use FinGather\Controller\PortfolioDataController;
 use FinGather\Dto\Enum\RangeEnum;
-use FinGather\Model\Entity\Enum\TransactionActionTypeEnum;
-use FinGather\Model\Repository\Enum\OrderDirectionEnum;
-use FinGather\Model\Repository\Enum\TransactionOrderByEnum;
+use FinGather\Middleware\AuthorizationMiddleware;
 use FinGather\Service\Provider\DataProvider;
 use FinGather\Service\Provider\PortfolioDataProvider;
 use FinGather\Service\Provider\PortfolioProvider;
-use FinGather\Service\Provider\TransactionProvider;
 use FinGather\Service\Provider\UserProvider;
-use FinGather\Utils\DateTimeUtils;
-use Safe\DateTimeImmutable;
+use GuzzleHttp\Psr7\ServerRequest;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use function Safe\hrtime;
@@ -45,8 +42,8 @@ final class BenchmarkPortfolioValueCommand extends AbstractCommand
 		$portfolioDataProvider = $application->container->get(PortfolioDataProvider::class);
 		assert($portfolioDataProvider instanceof PortfolioDataProvider);
 
-		$transactionProvider = $application->container->get(TransactionProvider::class);
-		assert($transactionProvider instanceof TransactionProvider);
+		$portfolioDataController = $application->container->get(PortfolioDataController::class);
+		assert($portfolioDataController instanceof PortfolioDataController);
 
 		$userId = 2;
 		$user = $userProvider->getUser($userId);
@@ -57,35 +54,14 @@ final class BenchmarkPortfolioValueCommand extends AbstractCommand
 		$dataProvider->deleteData(user: $user);
 		$portfolio = $portfolioProvider->getDefaultPortfolio($user);
 
+		$serverRequest = new ServerRequest('GET', '/api/portfolio-data-range/' . $portfolio->getId());
+		$serverRequest = $serverRequest->withAttribute(AuthorizationMiddleware::AttributeUser, $user);
+		$serverRequest = $serverRequest->withQueryParams(['range' => RangeEnum::All->value]);
+
 		//BEGIN
 		$timeStart = hrtime(true);
 
-		$transactions = $transactionProvider->getTransactions(
-			user: $user,
-			portfolio: $portfolio,
-			actionTypes: [TransactionActionTypeEnum::Buy, TransactionActionTypeEnum::Sell],
-			orderBy: [TransactionOrderByEnum::ActionCreated->value => OrderDirectionEnum::ASC],
-		);
-
-		if (count($transactions) === 0) {
-			throw new \Exception('No transactions found.');
-		}
-
-		$firstTransaction = $transactions[array_key_first($transactions)];
-
-		$range = RangeEnum::All;
-
-		$datePeriod = DateTimeUtils::getDatePeriod(
-			range: $range,
-			firstDate: $firstTransaction->getActionCreated(),
-			shiftStartDate: true,
-		);
-		foreach ($datePeriod as $dateTime) {
-			/** @var \DateTimeImmutable $dateTime */
-			$dateTimeConverted = DateTimeImmutable::createFromRegular($dateTime);
-
-			$portfolioDataProvider->getPortfolioData($user, $portfolio, $dateTimeConverted);
-		}
+		$portfolioDataController->actionGetPortfolioDataRange($serverRequest, $portfolio->getId());
 
 		$timeEnd = hrtime(true);
 		//END
