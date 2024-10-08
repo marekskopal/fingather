dafded1df6145bff97ffe769180a3ee6223c02d9
Marek Skopal <skopal.marek@gmail.com>
2024-10-08 18:45:25 +0200

    Add clear cache storage command

--- backend/src/Command/CacheClearCommand.php
+++ backend/src/Command/CacheClearCommand.php
@@ -5,22 +5,51 @@ declare(strict_types=1);
 namespace FinGather\Command;
 
 use FinGather\Service\Cache\CacheFactory;
+use FinGather\Service\Cache\CacheStorageEnum;
+use Nette\Caching\Cache;
+use Predis\Client;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-final class CacheClearCommand extends Command
+final class CacheClearCommand extends AbstractCommand
 {
+	private const OptionStorage = 'storage';
+
 	protected function configure(): void
 	{
 		$this->setName('cache:clear');
+
+		$this->addOption(self::OptionStorage, 's', InputOption::VALUE_OPTIONAL, 'Storage type (memcached, redis)');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
-		$cache = CacheFactory::createPsrCache();
-		$cache->clear();
+		$client = new Client('tcp://' . getenv('REDIS_HOST') . ':' . getenv('REDIS_PORT'), [
+			'parameters' => [
+				'password' => (string) getenv('REDIS_PASSWORD'),
+			],
+		]);
 
-		return 0;
+		$cacheFactory = new CacheFactory($client);
+
+		/** @var string|null $storageOption */
+		$storageOption = $input->getOption(self::OptionStorage);
+
+		$storages = $storageOption === null ? [
+			CacheStorageEnum::Memcached,
+			CacheStorageEnum::Redis,
+		] : [
+			CacheStorageEnum::from($storageOption),
+		];
+
+		foreach ($storages as $storage) {
+			$this->writeln(sprintf('Clearing cache for storage: %s', $storage->value), $output);
+			$cache = $cacheFactory->create($storage);
+			$cache->clean([Cache::All => true]);
+		}
+
+		return self::SUCCESS;
 	}
 }
