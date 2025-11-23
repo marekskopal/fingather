a7e393c943eb732d1e1c82b860dd2b1517e8fe24
Marek Skopal <skopal.marek@gmail.com>
2025-11-23 14:07:24 +0100

    Fix phpstan

--- backend/src/Response/ConflictResponse.php
+++ backend/src/Response/ConflictResponse.php
@@ -6,7 +6,7 @@ namespace FinGather\Response;
 
 final class ConflictResponse extends ErrorResponse
 {
-	/** @param array<string> $headers */
+	/** @param array<non-empty-string, array<string>|string> $headers */
 	public function __construct(string $data, int $status = 409, array $headers = [], int $encodingOptions = self::DEFAULT_JSON_FLAGS)
 	{
 		parent::__construct($data, $status, $headers, $encodingOptions);
--- backend/src/Response/NotFoundResponse.php
+++ backend/src/Response/NotFoundResponse.php
@@ -6,7 +6,7 @@ namespace FinGather\Response;
 
 final class NotFoundResponse extends ErrorResponse
 {
-	/** @param array<string> $headers */
+	/** @param array<non-empty-string, array<string>|string> $headers */
 	public function __construct(string $data, int $status = 404, array $headers = [], int $encodingOptions = self::DEFAULT_JSON_FLAGS)
 	{
 		parent::__construct($data, $status, $headers, $encodingOptions);
