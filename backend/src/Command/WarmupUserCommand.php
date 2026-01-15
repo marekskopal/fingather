59f1b7f3d3f683c04e538ba18b18e0d8d78699cf
Marek Skopal <skopal.marek@gmail.com>
2026-01-15 23:03:25 +0100

    Add portfolioId to warmup command

--- backend/src/Command/WarmupUserCommand.php
+++ backend/src/Command/WarmupUserCommand.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace FinGather\Command;
 
 use FinGather\App\ApplicationFactory;
+use FinGather\Service\Provider\PortfolioProvider;
 use FinGather\Service\Provider\UserProvider;
 use FinGather\Service\Warmup\UserWarmup;
+use FinGather\Utils\BenchmarkUtils;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -15,10 +17,13 @@ final class WarmupUserCommand extends AbstractCommand
 {
 	private const string ArgumentUserId = 'userId';
 
+	private const string PortfolioId = 'portfolioId';
+
 	protected function configure(): void
 	{
 		$this->setName('warmup:user');
 		$this->addArgument(self::ArgumentUserId, InputArgument::REQUIRED, 'User ID');
+		$this->addArgument(self::PortfolioId, InputArgument::OPTIONAL, 'Portfolio ID');
 	}
 
 	protected function process(InputInterface $input, OutputInterface $output): int
@@ -30,6 +35,9 @@ final class WarmupUserCommand extends AbstractCommand
 
 		$userProvider = $application->container->get(UserProvider::class);
 		assert($userProvider instanceof UserProvider);
+
+		$portfolioProvider = $application->container->get(PortfolioProvider::class);
+		assert($portfolioProvider instanceof PortfolioProvider);
 
 		$userId = $input->getArgument(self::ArgumentUserId);
 		if (!is_numeric($userId)) {
@@ -43,7 +51,23 @@ final class WarmupUserCommand extends AbstractCommand
 			return self::FAILURE;
 		}
 
-		$userWarmup->warmup($user);
+		$portfolioId = $input->getArgument(self::PortfolioId);
+		if (!is_numeric($portfolioId)) {
+			$benchmarkTime = BenchmarkUtils::benchmark(fn() => $userWarmup->warmup($user));
+
+			$this->writeln('Warmup was finished - ' . $benchmarkTime . 'ms', $output);
+
+			return self::SUCCESS;
+		}
+
+		$portfolio = $portfolioProvider->getPortfolio($user, (int) $portfolioId);
+		if ($portfolio === null) {
+			$this->writeln('Portfolio not found.', $output);
+			return self::FAILURE;
+		}
+		$benchmarkTime = BenchmarkUtils::benchmark(fn() => $userWarmup->warmupPortfolio($user, $portfolio));
+
+		$this->writeln('Warmup was finished - ' . $benchmarkTime . 'ms', $output);
 
 		return self::SUCCESS;
 	}
